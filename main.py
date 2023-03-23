221b798b1355ab5f1f0506a3c96ef1cc7241636f
qralmeida <quinn.14march@gmail.com>
2023-03-23 17:15:16 +0100

    Separated data into appropriate subarrays

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-calibration_df = pd.read_csv('')
-
-wassup = True
+#calibration_df = pd.read_csv('')
 
 df = pd.read_csv('data_5hprobe.csv')
 column_headers = df.columns
 print(column_headers[0])
+
 data_array = df.to_numpy()
 
-ps = data_array[:1075,12]
-xs = data_array[:1075,15]
-ys = data_array[:1075,16]
+# separate data into regen and propulsive stage and into the 3 different conditions
+data_prop_stage = np.array_split(data_array[:1050,:],3)
+data_regen_stage = np.array_split(data_array[1050:,:],3)
+
+
+
+ps = data_array[:351,12]
+xs = data_array[:351,15]
+ys = data_array[:351,16]
 
 y_len = 0
 x_len = 0
@@ -29,7 +34,7 @@ for i in range(len(xs)-1):
     if xs[i] != xs[i+1]:
         x_len += 1
 
-print(x_len, y_len)
+#print(x_len, y_len)
 
 n=0
 data_matrix = np.empty((x_len, y_len, 3))
@@ -46,13 +51,13 @@ for i, row in enumerate(data_matrix):
         if n >= len(xs):
             break
         elif (xs[n] != xs[n-1]):
-            print(f'DIFFERENCE: {(xs[n-1], xs[n])}')
-            print(f'count: {n}, rowlength = {row_length}, {(i,j)}')
+            #print(f'DIFFERENCE: {(xs[n-1], xs[n])}')
+            #print(f'count: {n}, rowlength = {row_length}, {(i,j)}')
             assert row[0,0] == xs[n-1] and row_length<=35
 
             break
 
-print(data_matrix[:,:25,0])
+#print(data_matrix[:,:25,0])
 
 class cp_results:
     def __init__(self, cp_theta, cp_phi, cp_center, cp_ave):
@@ -90,12 +95,12 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(data_matrix[:,:,0], data_matrix[:,:,1], data_matrix[:,:,2], alpha = 1) #, lw=0.5, rstride=8, cstride=8, alpha=0.3)
 
 ax.set(xlabel='X position', ylabel='Y position', zlabel='Pressure')
-plt.savefig("plot1.png")
+#plt.savefig("plot1.png")
 # Plot projections of the contours for each dimension.  By choosing offsets
 # that match the appropriate axes limits, the projected contours will sit on
 # the 'walls' of the graph.
 #ax.contour(Xs, Ys, Zs, zdir='z', offset=-100, cmap='coolwarm')
 #ax.contour(Xs, Ys, Zs, zdir='x', offset=-40, cmap='coolwarm')
 #ax.contour(Xs, Ys, Zs, zdir='y', offset=40, cmap='coolwarm')
-plt.show()
+#plt.show()
 
